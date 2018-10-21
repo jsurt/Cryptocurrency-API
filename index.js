f52c89b6d2118b69e8d09fb59d3232ecb91b658f
Jack Surtees <jacksurtees@gmail.com>
2018-10-21 16:03:22 -0400

    updated js

--- index.js
+++ index.js
@@ -10,30 +10,49 @@ function ajaxCall() {
 function displayCoinData(data) {
     console.log(data);
     console.log(selectedMarket); 
-    $('#js-output').removeAttr('hidden').prepend(
+    $('#js-data-output').removeAttr('hidden').prepend(
         `<p class="js-market-name">${data.ticker.markets[selectedMarket].market}</p>
         <ul>
         <li class="js-coin-info">Price: $${data.ticker.markets[selectedMarket].price}</li>
         <li class="js-coin-info">Volume: ${data.ticker.markets[selectedMarket].volume}</li>
-        </ul>`
+        </ul>
+        <button id="new-search">Test</button>`
     )   
-    newSearch();
+    newSearch();    
 }
 
 function dataRequest() {
     $('#select-market-form').submit(function(event){
         event.preventDefault();
         selectedMarket = $('select').find('option:selected').attr('value');
-        $('#select-market-form, #subhead').toggleClass('hidden');
+        $('#select-market-form, header').toggleClass('hidden');
         ajaxCall();
-    })
+    });
+
 }
 
 function newSearch() {
-    $('#js-output').find('#new-search').click(function() {
-        $('#js-output').empty().attr('hidden', true)
-        $('#select-market-form, #subhead').toggleClass('hidden');
-    })
+    $('#js-data-output').find('#new-search').click(function() {
+        $('#js-data-output').empty().attr('hidden', true)
+        $('#select-market-form, header').toggleClass('hidden');
+    });
+    
 }
+
+function getTweets() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: 'https://api.twitter.com/1.1/search/tweets.json',
+        data: {
+            q: 'cats'
+        },
+        success: function() {
+            console.log(data);
+        }
+    });
+}
+
+getTweets();
 
 dataRequest();
